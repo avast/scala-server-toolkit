281b939414988566c432487635bac72878e3da20
Scala Steward <me@scala-steward.org>
2019-09-28 16:29:58 +0200

    Update pureconfig to 0.12.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
 
   val catsEffect = "org.typelevel" %% "cats-effect" % "2.0.0"
   val kindProjector = "org.typelevel" %% "kind-projector" % "0.10.3"
-  val pureConfig = "com.github.pureconfig" %% "pureconfig" % "0.12.0"
+  val pureConfig = "com.github.pureconfig" %% "pureconfig" % "0.12.1"
   val slf4jApi = "org.slf4j" % "slf4j-api" % "1.7.28"
   val zio = "dev.zio" %% "zio" % "1.0.0-RC13"
   val zioInteropCats = "dev.zio" %% "zio-interop-cats" % "2.0.0.0-RC4"
