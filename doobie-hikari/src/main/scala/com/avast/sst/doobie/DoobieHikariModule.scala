a3f508c5489cb80ea5061d65c4baa3f47fb1002d
Janecek Jakub <janecek.jakub@gmail.com>
2019-10-22 10:04:18 +0200

    refactor: Minor PR tweaks

--- doobie-hikari/src/main/scala/com/avast/sst/doobie/DoobieHikariModule.scala
+++ doobie-hikari/src/main/scala/com/avast/sst/doobie/DoobieHikariModule.scala
@@ -20,27 +20,26 @@ object DoobieHikariModule {
     metricsTrackerFactory: Option[MetricsTrackerFactory] = None
   )(implicit cs: ContextShift[F]): Resource[F, HikariTransactor[F]] = {
     for {
-      hikariConfig <- Resource.liftF {
-                       Sync[F].delay {
-                         val c = new HikariConfig()
-                         c.setDriverClassName(config.driver)
-                         c.setJdbcUrl(config.url)
-                         c.setUsername(config.username)
-                         c.setPassword(config.password)
-                         c.setAutoCommit(config.autoCommit)
-                         c.setConnectionTimeout(config.connectionTimeout.toMillis)
-                         c.setIdleTimeout(config.idleTimeout.toMillis)
-                         c.setMaxLifetime(config.maxLifeTime.toMillis)
-                         c.setMinimumIdle(config.minimumIdle)
-                         c.setMaximumPoolSize(config.maximumPoolSize)
-                         c.setReadOnly(config.readOnly)
-                         config.leakDetectionThreshold.map(_.toMillis).foreach(c.setLeakDetectionThreshold)
-                         metricsTrackerFactory.foreach(c.setMetricsTrackerFactory)
-                         c
-                       }
-                     }
+      hikariConfig <- Resource.liftF(Sync[F].delay(makeHikariConfig(config, metricsTrackerFactory)))
       transactor <- HikariTransactor.fromHikariConfig(hikariConfig, boundedConnectExecutionContext, blocker)
     } yield transactor
   }
 
+  private def makeHikariConfig(config: DoobieHikariConfig, metricsTrackerFactory: Option[MetricsTrackerFactory]) = {
+    val c = new HikariConfig()
+    c.setDriverClassName(config.driver)
+    c.setJdbcUrl(config.url)
+    c.setUsername(config.username)
+    c.setPassword(config.password)
+    c.setAutoCommit(config.autoCommit)
+    c.setConnectionTimeout(config.connectionTimeout.toMillis)
+    c.setIdleTimeout(config.idleTimeout.toMillis)
+    c.setMaxLifetime(config.maxLifeTime.toMillis)
+    c.setMinimumIdle(config.minimumIdle)
+    c.setMaximumPoolSize(config.maximumPoolSize)
+    c.setReadOnly(config.readOnly)
+    config.leakDetectionThreshold.map(_.toMillis).foreach(c.setLeakDetectionThreshold)
+    metricsTrackerFactory.foreach(c.setMetricsTrackerFactory)
+    c
+  }
 }
