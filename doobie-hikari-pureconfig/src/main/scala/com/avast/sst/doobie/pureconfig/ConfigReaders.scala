c4a8d732b30aca8c2e52bc13b6974ade19527a41
Janecek Jakub <janecek.jakub@gmail.com>
2019-10-22 20:56:42 +0200

    fix: ConfigReader for TransactionIsolation

--- doobie-hikari-pureconfig/src/main/scala/com/avast/sst/doobie/pureconfig/ConfigReaders.scala
+++ doobie-hikari-pureconfig/src/main/scala/com/avast/sst/doobie/pureconfig/ConfigReaders.scala
@@ -1,13 +1,22 @@
 package com.avast.sst.doobie.pureconfig
 
+import cats.syntax.either._
 import com.avast.sst.doobie.DoobieHikariConfig
 import doobie.enum.TransactionIsolation
 import pureconfig.ConfigReader
-import pureconfig.generic.semiauto.{deriveEnumerationReader, deriveReader}
+import pureconfig.error.CannotConvert
+import pureconfig.generic.semiauto.deriveReader
 
 trait ConfigReaders {
 
-  implicit val transactionIsolationReader: ConfigReader[TransactionIsolation] = deriveEnumerationReader
+  implicit val transactionIsolationReader: ConfigReader[TransactionIsolation] = ConfigReader[String].emap {
+    case "TRANSACTION_NONE"             => TransactionIsolation.TransactionNone.asRight
+    case "TRANSACTION_READ_UNCOMMITTED" => TransactionIsolation.TransactionReadUncommitted.asRight
+    case "TRANSACTION_READ_COMMITTED"   => TransactionIsolation.TransactionReadCommitted.asRight
+    case "TRANSACTION_REPEATABLE_READ"  => TransactionIsolation.TransactionRepeatableRead.asRight
+    case "TRANSACTION_SERIALIZABLE"     => TransactionIsolation.TransactionSerializable.asRight
+    case unknown                        => Left(CannotConvert(unknown, "TransactionIsolation", "unknown value"))
+  }
 
   implicit val configReader: ConfigReader[DoobieHikariConfig] = deriveReader
 
