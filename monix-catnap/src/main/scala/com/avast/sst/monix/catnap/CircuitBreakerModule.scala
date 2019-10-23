f08f35ebb5703cd071e44690ca2562735b3664ad
Janecek Jakub <janecek.jakub@gmail.com>
2019-10-23 16:34:25 +0200

    refactor: Make Clock dependency non-optional

--- monix-catnap/src/main/scala/com/avast/sst/monix/catnap/CircuitBreakerModule.scala
+++ monix-catnap/src/main/scala/com/avast/sst/monix/catnap/CircuitBreakerModule.scala
@@ -6,9 +6,8 @@ import monix.catnap.CircuitBreaker
 object CircuitBreakerModule {
 
   /** Makes [[monix.catnap.CircuitBreaker]] initialized with the given config and optionally [[cats.effect.Clock]]. */
-  def make[F[_]: Sync](config: CircuitBreakerConfig, clock: Option[Clock[F]] = None): F[CircuitBreaker[F]] = {
-    implicit val finalClock: Clock[F] = clock.getOrElse(Clock.create[F])
-    CircuitBreaker.of[F](config.maxFailures, config.resetTimeout, config.exponentialBackoffFactor, config.maxResetTimeout)
+  def make[F[_]: Sync](config: CircuitBreakerConfig, clock: Clock[F]): F[CircuitBreaker[F]] = {
+    CircuitBreaker.of[F](config.maxFailures, config.resetTimeout, config.exponentialBackoffFactor, config.maxResetTimeout)(Sync[F], clock)
   }
 
 }
