0e7cb95865c7f4e554cfb71acab964ba38dbb13f
Janecek Jakub <janecek.jakub@gmail.com>
2019-10-23 16:43:03 +0200

    fix: ScalaDoc

--- monix-catnap/src/main/scala/com/avast/sst/monix/catnap/CircuitBreakerModule.scala
+++ monix-catnap/src/main/scala/com/avast/sst/monix/catnap/CircuitBreakerModule.scala
@@ -5,7 +5,7 @@ import monix.catnap.CircuitBreaker
 
 object CircuitBreakerModule {
 
-  /** Makes [[monix.catnap.CircuitBreaker]] initialized with the given config and optionally [[cats.effect.Clock]]. */
+  /** Makes [[monix.catnap.CircuitBreaker]] initialized with the given config and [[cats.effect.Clock]]. */
   def make[F[_]: Sync](config: CircuitBreakerConfig, clock: Clock[F]): F[CircuitBreaker[F]] = {
     CircuitBreaker.of[F](config.maxFailures, config.resetTimeout, config.exponentialBackoffFactor, config.maxResetTimeout)(Sync[F], clock)
   }
