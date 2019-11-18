9517d1b022a78958e72686ad70f413d05ca5c963
Scala Steward <me@scala-steward.org>
2019-11-18 06:02:57 +0100

    Update zio to 1.0.0-RC17

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -27,7 +27,7 @@ object Dependencies {
   val silencerLib = "com.github.ghik" % "silencer-lib" % Versions.silencer cross CrossVersion.full
   val slf4jApi = "org.slf4j" % "slf4j-api" % "1.7.29"
   val sslConfig = "com.typesafe" %% "ssl-config-core" % "0.4.1"
-  val zio = "dev.zio" %% "zio" % "1.0.0-RC16"
+  val zio = "dev.zio" %% "zio" % "1.0.0-RC17"
   val zioInteropCats = "dev.zio" %% "zio-interop-cats" % "2.0.0.0-RC7"
 
   object Versions {
