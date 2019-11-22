b02a3189afd7c39512e897497022f9b07bd003aa
amigarulez <amigarulez@protonmail.com>
2019-11-22 13:22:33 +0100

    refactor: remove unused ssl classes

--- datastax-java-driver/src/main/scala/com/avast/sst/datastax/ssl/KeyStoreConfig.scala
+++
@@ -1,5 +0,0 @@
-package com.avast.sst.datastax.ssl
-
-import java.nio.file.Path
-
-final case class KeyStoreConfig(keystoreType: KeyStoreType, path: Path, password: String, keyPassword: Option[String])
--- datastax-java-driver/src/main/scala/com/avast/sst/datastax/ssl/SslContextConfig.scala
+++
@@ -1,9 +0,0 @@
-package com.avast.sst.datastax.ssl
-
-import com.avast.sst.datastax.ssl.Protocol.TLS
-
-final case class SslContextConfig(protocol: Protocol = TLS,
-                                  keystore: Option[KeyStoreConfig] = None,
-                                  truststore: Option[KeyStoreConfig] = None,
-                                  loadSystemKeyManagers: Boolean = true,
-                                  loadSystemTrustManagers: Boolean = true)
