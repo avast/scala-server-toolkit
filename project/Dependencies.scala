4922c2ce576fa925f8174b269eda56b1353636dc
Scala Steward <me@scala-steward.org>
2019-11-28 18:20:10 +0100

    Update zio-interop-cats to 2.0.0.0-RC10

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -29,7 +29,7 @@ object Dependencies {
   val slf4jApi = "org.slf4j" % "slf4j-api" % "1.7.29"
   val sslConfig = "com.typesafe" %% "ssl-config-core" % "0.4.1"
   val zio = "dev.zio" %% "zio" % "1.0.0-RC17"
-  val zioInteropCats = "dev.zio" %% "zio-interop-cats" % "2.0.0.0-RC9"
+  val zioInteropCats = "dev.zio" %% "zio-interop-cats" % "2.0.0.0-RC10"
 
   object Versions {
 
