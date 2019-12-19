a3302b925af736eeee55c88c91fb01c1180473b4
Jan Strnad <strnad@avast.com>
2019-12-19 10:01:55 +0100

    feat: Use reference config for ssl context

--- ssl-config/src/main/scala/com/avast/sst/ssl/SslContextModule.scala
+++ ssl-config/src/main/scala/com/avast/sst/ssl/SslContextModule.scala
@@ -1,7 +1,7 @@
 package com.avast.sst.ssl
 
 import cats.effect.Sync
-import com.typesafe.config.Config
+import com.typesafe.config.{Config, ConfigFactory}
 import com.typesafe.sslconfig.ssl.{
   ConfigSSLContextBuilder,
   DefaultKeyManagerFactoryWrapper,
@@ -14,11 +14,14 @@ import scala.language.higherKinds
 
 object SslContextModule {
 
+  private[this] val referenceConfig = ConfigFactory.defaultReference().getConfig("ssl-config")
+
   /** Initializes [[javax.net.ssl.SSLContext]] from the provided config. */
-  def make[F[_]: Sync](config: Config): F[SSLContext] = Sync[F].delay {
+  def make[F[_]: Sync](config: Config, withReference: Boolean = true): F[SSLContext] = Sync[F].delay {
     val loggerFactory = Slf4jLogger.factory
+    val finalConfig = if (withReference) config.withFallback(referenceConfig) else config
     new ConfigSSLContextBuilder(loggerFactory,
-                                SSLConfigFactory.parse(config, loggerFactory),
+                                SSLConfigFactory.parse(finalConfig, loggerFactory),
                                 new DefaultKeyManagerFactoryWrapper(KeyManagerFactory.getDefaultAlgorithm),
                                 new DefaultTrustManagerFactoryWrapper(TrustManagerFactory.getDefaultAlgorithm)).build
   }
