48c69c1b51255488e29d3f875bb6ece8d7cebced
Ondra Pelech <ondra.pelech@gmail.com>
2020-01-10 13:37:07 +0100

    address PR comments

--- jvm/src/main/scala/com/avast/sst/jvm/execution/ConfigurableThreadFactory.scala
+++ jvm/src/main/scala/com/avast/sst/jvm/execution/ConfigurableThreadFactory.scala
@@ -10,9 +10,7 @@ import com.avast.sst.jvm.execution.ConfigurableThreadFactory.Config
 /** Thread factory (both [[java.util.concurrent.ThreadFactory]] and [[java.util.concurrent.ForkJoinPool.ForkJoinWorkerThreadFactory]])
   * that creates new threads according to the provided [[com.avast.sst.jvm.execution.ConfigurableThreadFactory.Config]].
   */
-class ConfigurableThreadFactory(config: Config, uncaughtExceptionHandler: UncaughtExceptionHandler = LoggingUncaughtExceptionHandler)
-    extends ThreadFactory
-    with ForkJoinWorkerThreadFactory {
+class ConfigurableThreadFactory(config: Config) extends ThreadFactory with ForkJoinWorkerThreadFactory {
 
   private val counter = new AtomicLong(0L)
 
@@ -26,7 +24,7 @@ class ConfigurableThreadFactory(config: Config, uncaughtExceptionHandler: Uncaug
     config.nameFormat.map(_.format(counter.getAndIncrement())).foreach(thread.setName)
     thread.setDaemon(config.daemon)
     thread.setPriority(config.priority)
-    thread.setUncaughtExceptionHandler(uncaughtExceptionHandler)
+    thread.setUncaughtExceptionHandler(config.uncaughtExceptionHandler)
     thread
   }
 
@@ -37,6 +35,9 @@ object ConfigurableThreadFactory {
   /**
     * @param nameFormat Formatted with long number, e.g. my-thread-%02d
     */
-  final case class Config(nameFormat: Option[String] = None, daemon: Boolean = false, priority: Int = Thread.NORM_PRIORITY)
+  final case class Config(nameFormat: Option[String] = None,
+                          daemon: Boolean = false,
+                          priority: Int = Thread.NORM_PRIORITY,
+                          uncaughtExceptionHandler: UncaughtExceptionHandler = LoggingUncaughtExceptionHandler)
 
 }
--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -24,8 +24,8 @@ object BuildSettings {
       Wart.DefaultArguments // for constructors for PureConfig
     ),
     ThisBuild / scalafixDependencies ++= Seq(
-      Dependencies.scalazzi, // https://github.com/scalaz/scalazzi
-      Dependencies.sortImports // https://github.com/NeQuissimus/sort-imports
+      Dependencies.scalazzi,
+      Dependencies.sortImports
     ),
     scalacOptions ++= Seq(
       "-Yrangepos", // for scalafix. required by SemanticDB compiler plugin
