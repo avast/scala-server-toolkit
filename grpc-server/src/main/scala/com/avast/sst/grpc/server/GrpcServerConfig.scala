71421fa521a212414c40cda425305040b38fe3bc
Janecek Jakub <janecek.jakub@gmail.com>
2020-02-10 11:54:02 +0100

    fix: Missing default value for handshakeTimeout in GrpcServerConfig

--- grpc-server/src/main/scala/com/avast/sst/grpc/server/GrpcServerConfig.scala
+++ grpc-server/src/main/scala/com/avast/sst/grpc/server/GrpcServerConfig.scala
@@ -5,7 +5,7 @@ import java.util.concurrent.TimeUnit
 import scala.concurrent.duration.Duration
 
 final case class GrpcServerConfig(port: Int,
-                                  handshakeTimeout: Duration,
+                                  handshakeTimeout: Duration = Duration(120, TimeUnit.SECONDS),
                                   maxInboundMessageSize: Int = 4 * 1024 * 1024,
                                   maxInboundMetadataSize: Int = 8192,
                                   serverShutdownTimeout: Duration = Duration(10, TimeUnit.SECONDS))
