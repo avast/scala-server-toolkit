06846ece37cfb3105e0285df9c5b7c5773a60d70
Jakub Janeček <janecek.jakub@gmail.com>
2020-03-19 09:36:26 +0100

    Update/sbt scalafix 0.9.12 (#182)
    * Update sbt-scalafix to 0.9.12
    * fix: Compile warning after sbt-scalafix upgrade
    
    Co-authored-by: Scala Steward <me@scala-steward.org>

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -25,6 +25,7 @@ object Dependencies {
   val monixEval = "io.monix" %% "monix-eval" % Versions.monix
   val postgresql = "org.postgresql" % "postgresql" % "42.2.11"
   val pureConfig = "com.github.pureconfig" %% "pureconfig" % "0.12.3"
+  val scalaCollectionCompat = "org.scala-lang.modules" %% "scala-collection-compat" % "2.1.4"
   val scalafixScaluzzi = "com.github.vovapolu" %% "scaluzzi" % "0.1.3"
   val scalafixSortImports = "com.nequissimus" %% "sort-imports" % "0.3.2"
   val scalaTest = "org.scalatest" %% "scalatest" % "3.1.1"
