bc6f0bfef31964c83c01cdacd2cfde4513dfab06
Janecek Jakub <janecek.jakub@gmail.com>
2020-03-25 11:12:05 +0100

    feat: Implement gRPC server logging and monitoring interceptors

---
+++ grpc-server/src/main/scala/com/avast/sst/grpc/server/interceptor/LoggingServerInterceptor.scala
@@ -0,0 +1,48 @@
+package com.avast.sst.grpc.server.interceptor
+
+import io.grpc.ForwardingServerCall.SimpleForwardingServerCall
+import io.grpc.ForwardingServerCallListener.SimpleForwardingServerCallListener
+import io.grpc._
+import org.slf4j.Logger
+
+/** Adds basic logging around each gRPC call. */
+class LoggingServerInterceptor(logger: Logger) extends ServerInterceptor {
+
+  override def interceptCall[ReqT, RespT](
+      call: ServerCall[ReqT, RespT],
+      headers: Metadata,
+      next: ServerCallHandler[ReqT, RespT]
+  ): ServerCall.Listener[ReqT] = {
+    val methodName = call.getMethodDescriptor.getFullMethodName
+    val finalCall = new CloseServerCall(methodName, call)
+    new OnMessageServerCallListener(methodName, next.startCall(finalCall, headers))
+  }
+
+  private class CloseServerCall[A, B](methodName: String, delegate: ServerCall[A, B]) extends SimpleForwardingServerCall[A, B](delegate) {
+    override def close(status: Status, trailers: Metadata): Unit = {
+      if (!status.isOk) {
+        logger.error(
+          String.format(
+            "Error response from method %s: %s %s",
+            methodName,
+            status.getCode,
+            status.getDescription
+          ),
+          status.getCause
+        )
+      } else {
+        logger.debug("Successful response from method {}: {}", Array(methodName, status): _*)
+      }
+      super.close(status, trailers)
+    }
+  }
+
+  private class OnMessageServerCallListener[A](methodName: String, delegate: ServerCall.Listener[A])
+      extends SimpleForwardingServerCallListener[A](delegate) {
+    override def onMessage(message: A): Unit = {
+      logger.debug("Dispatching method {}", methodName)
+      super.onMessage(message)
+    }
+  }
+
+}
