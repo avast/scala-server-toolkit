fc2b77ed3025cd38a8b7c920432d8d45b708f72c
Janecek Jakub <janecek.jakub@gmail.com>
2020-05-20 23:13:39 +0200

    build: Disable MiMa for now

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -25,8 +25,6 @@ object BuildSettings {
     scalaVersion := "2.13.2",
     crossScalaVersions := List(scalaVersion.value, "2.12.11"),
     fork := true,
-    mimaPreviousArtifacts ~= { _.filter(_.revision == "0.1.34") }, // this is just temporary until we have release version 1.x
-    mimaFailOnNoPrevious := false,
     libraryDependencies ++= Seq(
       compilerPlugin(Dependencies.kindProjector),
       compilerPlugin(Dependencies.silencer),
