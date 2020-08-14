04e90d9047a78510224906f0c70c058effcc60c5
Scala Steward <me@scala-steward.org>
2020-08-14 22:04:31 +0200

    Update postgresql to 42.2.15

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -24,7 +24,7 @@ object Dependencies {
   val micrometerStatsD = "io.micrometer" % "micrometer-registry-statsd" % Versions.micrometerStatsD
   val monixCatnap = "io.monix" %% "monix-catnap" % Versions.monix
   val monixEval = "io.monix" %% "monix-eval" % Versions.monix
-  val postgresql = "org.postgresql" % "postgresql" % "42.2.14"
+  val postgresql = "org.postgresql" % "postgresql" % "42.2.15"
   val pureConfig = "com.github.pureconfig" %% "pureconfig" % "0.13.0"
   val scalaCollectionCompat = "org.scala-lang.modules" %% "scala-collection-compat" % "2.1.6"
   val scalafixScaluzzi = "com.github.vovapolu" %% "scaluzzi" % "0.1.12"
