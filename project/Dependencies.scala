39f69a6e3c87b3aeb168c0ee35fcf812390a71d3
Scala Steward <me@scala-steward.org>
2020-11-05 14:12:56 +0100

    Update sort-imports to 0.5.5

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -28,7 +28,7 @@ object Dependencies {
   val pureConfig = "com.github.pureconfig" %% "pureconfig" % "0.14.0"
   val scalaCollectionCompat = "org.scala-lang.modules" %% "scala-collection-compat" % "2.2.0"
   val scalafixScaluzzi = "com.github.vovapolu" %% "scaluzzi" % "0.1.15"
-  val scalafixSortImports = "com.nequissimus" %% "sort-imports" % "0.5.4"
+  val scalafixSortImports = "com.nequissimus" %% "sort-imports" % "0.5.5"
   val scalaTest = "org.scalatest" %% "scalatest" % "3.2.2"
   val sentry = "io.sentry" % "sentry" % "3.1.2"
   val silencer = "com.github.ghik" % "silencer-plugin" % Versions.silencer cross CrossVersion.full
