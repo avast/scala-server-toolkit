0c14eabf7d28c99d2647789af9b1dfb2a6930a92
Scala Steward <me@scala-steward.org>
2020-11-21 04:10:04 +0100

    Update scala-collection-compat to 2.3.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -27,7 +27,7 @@ object Dependencies {
   val monixEval = "io.monix" %% "monix-eval" % Versions.monix
   val postgresql = "org.postgresql" % "postgresql" % "42.2.18"
   val pureConfig = "com.github.pureconfig" %% "pureconfig" % "0.14.0"
-  val scalaCollectionCompat = "org.scala-lang.modules" %% "scala-collection-compat" % "2.2.0"
+  val scalaCollectionCompat = "org.scala-lang.modules" %% "scala-collection-compat" % "2.3.0"
   val scalafixScaluzzi = "com.github.vovapolu" %% "scaluzzi" % "0.1.15"
   val scalafixSortImports = "com.nequissimus" %% "sort-imports" % "0.5.5"
   val scalaTest = "org.scalatest" %% "scalatest" % "3.2.3"
