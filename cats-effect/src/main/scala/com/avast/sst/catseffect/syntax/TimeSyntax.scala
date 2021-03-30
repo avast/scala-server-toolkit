b8c2342d96cfc2787b8ab5762352fbf858839ee0
Scala Steward <me@scala-steward.org>
2021-03-30 08:47:32 +0200

    Applied Scalafix rule(s) github:typelevel/cats-effect/v3_0_0?sha=series/3.x

--- cats-effect/src/main/scala/com/avast/sst/catseffect/syntax/TimeSyntax.scala
+++ cats-effect/src/main/scala/com/avast/sst/catseffect/syntax/TimeSyntax.scala
@@ -1,10 +1,11 @@
 package com.avast.sst.catseffect.syntax
 
-import cats.effect.{Bracket, Clock}
+import cats.effect.Clock
 import com.avast.sst.catseffect.TimeUtils
 import com.avast.sst.catseffect.syntax.TimeSyntax.FOps
 
 import scala.concurrent.duration.Duration
+import cats.effect.MonadCancel
 
 trait TimeSyntax {
 
@@ -18,13 +19,13 @@ object TimeSyntax {
   final class FOps[F[_], A](private val f: F[A]) extends AnyVal {
 
     /** Measures the time it takes the effect to finish and records it using the provided function. */
-    def time(record: Duration => F[Unit])(implicit F: Bracket[F, Throwable], C: Clock[F]): F[A] = TimeUtils.time(f)(record)
+    def time(record: Duration => F[Unit])(implicit F: MonadCancel[F, Throwable], C: Clock[F]): F[A] = TimeUtils.time(f)(record)
 
     /** Measures the time it takes the effect to finish and records it using the provided function. It distinguishes between successful
       * and failure state.
       * Please note, that in case of the effect cancellation the `record` is not invoked at all.
       */
-    def timeCase(record: Either[Duration, Duration] => F[Unit])(implicit F: Bracket[F, Throwable], C: Clock[F]): F[A] = {
+    def timeCase(record: Either[Duration, Duration] => F[Unit])(implicit F: MonadCancel[F, Throwable], C: Clock[F]): F[A] = {
       TimeUtils.timeCase(f)(record)
     }
 
