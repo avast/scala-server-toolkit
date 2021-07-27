fe905edc8557607b0a27028db595346253a0bf29
Scala Steward <me@scala-steward.org>
2021-07-27 20:05:54 +0200

    Update zio from 1.0.9 to 1.0.10

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -37,7 +37,7 @@ object Dependencies {
   val sslConfig = "com.typesafe" %% "ssl-config-core" % "0.4.3"
   val testContainersScalaScalaTest = "com.dimafeng" %% "testcontainers-scala-scalatest" % "0.39.5"
   val testContainersScalaKafka = "com.dimafeng" %% "testcontainers-scala-kafka" % "0.39.5"
-  val zio = "dev.zio" %% "zio" % "1.0.9"
+  val zio = "dev.zio" %% "zio" % "1.0.10"
   val zioInteropCats = "dev.zio" %% "zio-interop-cats" % "2.5.1.0"
 
   object Versions {
