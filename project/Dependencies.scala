b643c32d33a08d67a161b8b4bdd891de0562061c
Scala Steward <me@scala-steward.org>
2021-07-30 16:08:33 +0200

    Update fs2-kafka from 1.7.0 to 2.1.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
   val doobie = "org.tpolecat" %% "doobie-core" % Versions.doobie
   val doobieHikari = "org.tpolecat" %% "doobie-hikari" % Versions.doobie
   val flywayCore = "org.flywaydb" % "flyway-core" % "7.12.0"
-  val fs2Kafka = "com.github.fd4s" %% "fs2-kafka" % "1.7.0"
+  val fs2Kafka = "com.github.fd4s" %% "fs2-kafka" % "2.1.0"
   val grpcNettyShaded = "io.grpc" % "grpc-netty-shaded" % Versions.grpc
   val grpcProtobuf = "io.grpc" % "grpc-protobuf" % Versions.grpc
   val grpcStub = "io.grpc" % "grpc-stub" % Versions.grpc
