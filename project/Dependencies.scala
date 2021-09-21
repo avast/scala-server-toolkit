be9627f17b216542f8be966b0854e3f3ca5d73a2
Scala Steward <me@scala-steward.org>
2021-09-21 02:06:55 +0200

    Update zio-interop-cats from 2.5.1.0 to 3.1.1.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -38,7 +38,7 @@ object Dependencies {
   val testContainersScalaScalaTest = "com.dimafeng" %% "testcontainers-scala-scalatest" % "0.39.8"
   val testContainersScalaKafka = "com.dimafeng" %% "testcontainers-scala-kafka" % "0.39.8"
   val zio = "dev.zio" %% "zio" % "1.0.12"
-  val zioInteropCats = "dev.zio" %% "zio-interop-cats" % "2.5.1.0"
+  val zioInteropCats = "dev.zio" %% "zio-interop-cats" % "3.1.1.0"
 
   object Versions {
 
