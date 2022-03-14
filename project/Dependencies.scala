92c55b7579f1254c4f5ad6040a61725c4eeabe85
Scala Steward <me@scala-steward.org>
2022-03-14 20:06:25 +0100

    Revert commit(s) 9cd8dca

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -46,7 +46,7 @@ object Dependencies {
     val datastaxJavaDriverCore = "4.14.0"
     val doobie = "0.13.4"
     val grpc = "1.45.0"
-    val http4s = "0.23.10"
+    val http4s = "0.22.11"
     val micrometerCore = "1.8.3"
     val micrometerJmx = "1.8.3"
     val micrometerStatsD = "1.8.3"
