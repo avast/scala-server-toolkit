b382065561697068d99f3e2476efab026b82eef2
Scala Steward <me@scala-steward.org>
2022-04-24 21:04:17 +0200

    Update testcontainers-scala-kafka, ... from 0.40.5 to 0.40.6

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -38,8 +38,8 @@ object Dependencies {
   val sentry = "io.sentry" % "sentry" % "5.7.3"
   val slf4jApi = "org.slf4j" % "slf4j-api" % "1.7.36"
   val sslConfig = "com.typesafe" %% "ssl-config-core" % "0.6.1"
-  val testContainersScalaScalaTest = "com.dimafeng" %% "testcontainers-scala-scalatest" % "0.40.5"
-  val testContainersScalaKafka = "com.dimafeng" %% "testcontainers-scala-kafka" % "0.40.5"
+  val testContainersScalaScalaTest = "com.dimafeng" %% "testcontainers-scala-scalatest" % "0.40.6"
+  val testContainersScalaKafka = "com.dimafeng" %% "testcontainers-scala-kafka" % "0.40.6"
   val zio = "dev.zio" %% "zio" % "1.0.14"
   val zioInteropCats = "dev.zio" %% "zio-interop-cats" % "2.5.1.0"
 
