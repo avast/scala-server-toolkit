6d3978be3ab2af278e827a34f4ed48090160fbf4
Scala Steward <scala_steward@virtuslab.com>
2022-07-13 13:41:30 +0000

    Update lettuce-core from 6.1.8.RELEASE to 6.1.9.RELEASE

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -20,7 +20,7 @@ object Dependencies {
   val jetbrainsAnnotations = "org.jetbrains" % "annotations" % "23.0.0"
   val jsr305 = "com.google.code.findbugs" % "jsr305" % "3.0.2"
   val kindProjector = "org.typelevel" % "kind-projector" % "0.13.2" cross CrossVersion.full
-  val lettuce = "io.lettuce" % "lettuce-core" % "6.1.8.RELEASE"
+  val lettuce = "io.lettuce" % "lettuce-core" % "6.1.9.RELEASE"
   val logbackClassic = "ch.qos.logback" % "logback-classic" % "1.2.11"
   val micrometerCore = "io.micrometer" % "micrometer-core" % Versions.micrometerCore
   val micrometerJmx = "io.micrometer" % "micrometer-registry-jmx" % Versions.micrometerJmx
