c7ead2254aeebd1a508617bde4acf8de224c778f
Scala Steward <scala_steward@virtuslab.com>
2024-05-29 13:20:29 +0000

    Update http4s-blaze-client, ... from 0.22.14 to 0.22.15

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -50,7 +50,7 @@ object Dependencies {
     val datastaxJavaDriverCore = "4.17.0"
     val doobie = "0.13.4"
     val grpc = "1.64.0"
-    val http4s = "0.22.14"
+    val http4s = "0.22.15"
     val micrometerCore = "1.10.13"
     val micrometerJmx = "1.10.13"
     val micrometerStatsD = "1.10.13"
