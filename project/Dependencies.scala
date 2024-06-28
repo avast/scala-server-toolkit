5f58d1cf67a0fc9955d7fb4715d0f2389f957c85
Scala Steward <scala_steward@virtuslab.com>
2024-06-28 14:12:02 +0000

    Update grpc-netty-shaded, grpc-protobuf, ... from 1.64.0 to 1.65.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -51,7 +51,7 @@ object Dependencies {
 
     val datastaxJavaDriverCore = "4.17.0"
     val doobie = "0.13.4"
-    val grpc = "1.64.0"
+    val grpc = "1.65.0"
     val http4s = "0.22.15"
     val micrometerCore = "1.12.7"
     val micrometerJmx = "1.12.7"
