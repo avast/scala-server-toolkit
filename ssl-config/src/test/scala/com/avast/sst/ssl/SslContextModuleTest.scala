b45c996ac6f6ea366cc02c4ca81dcae08642d500
Matthias Kurz <m.kurz@irregular.at>
2025-10-22 15:13:27 +0200

    Upgrade ssl-config-core to latest 0.7.0

--- ssl-config/src/test/scala/com/avast/sst/ssl/SslContextModuleTest.scala
+++ ssl-config/src/test/scala/com/avast/sst/ssl/SslContextModuleTest.scala
@@ -8,12 +8,12 @@ class SslContextModuleTest extends AnyFunSuite {
 
   test("SslContextModule initializes properly from JKS store with reference config") {
     val sslContext = SslContextModule.make[SyncIO](ConfigFactory.empty()).unsafeRunSync()
-    assert(sslContext.getProtocol === "TLSv1.2")
+    assert(sslContext.getProtocol === "TLSv1.3")
   }
 
   test("SslContextModule initializes properly from JKS store with provided config") {
     val sslContext = SslContextModule.make[SyncIO](ConfigFactory.load().getConfig("ssl-config"), withReference = false).unsafeRunSync()
-    assert(sslContext.getProtocol === "TLSv1.2")
+    assert(sslContext.getProtocol === "TLSv1.3")
   }
 
   test("SslContextModule fails to initialize for empty config and no reference config") {
